71e2858ac3c3806efd6fc8a3a5af5ab8f9a9a18b
Yota Nagahata <swimy_inf@yahoo.co.jp>
2017-01-09 18:22:12 +0900

    fix confirm script

--- text_analyze/doc2vec_scripts/confirm.py
+++ text_analyze/doc2vec_scripts/confirm.py
@@ -21,6 +21,37 @@ if len(args) == 1:
 REP_ROOT = os.environ['YA_RECOGNIZE_ROOT']
 TEXT_ANALYZE = REP_ROOT + '/text_analyze'
 
-loaded_model = models.doc2vec.Doc2Vec.load(TEXT_ANALYZE + '/doc2vec_model/model.d2c')
+conn = MySQLdb.connect(
+    host="localhost",
+    user=os.environ['YAHOO_AUCTION_DB_USERNAME'],
+    passwd=os.environ['YAHOO_AUCTION_DB_PASSWORD'],
+    db=os.environ['YAHOO_AUCTION_DB_NAME'],
+    charset = "utf8",
+    use_unicode=True,
+    cursorclass=MySQLdb.cursors.DictCursor
+)
+cursor = conn.cursor()
 
-print(loaded_model.docvecs.most_similar(args[1]))
+loaded_model = models.doc2vec.Doc2Vec.load(TEXT_ANALYZE + '/doc2vec_model/model.d2c')
+# print(loaded_model.docvecs.doctags)
+similarities = loaded_model.docvecs.most_similar(args[1])
+print("auction_id: {0}の類似度ランキング↓".format(args[1]))
+
+query = """
+    SELECT title, description FROM items where auction_id = '{0}';
+""".format(args[1])
+cursor.execute(query)
+item = cursor.fetchone()
+print("auction_id: {0}".format(args[1]))
+print("  title: {0}".format(item['title']))
+
+for aid, degree in similarities:
+    query = """
+        SELECT title, description FROM items where auction_id = '{0}';
+    """.format(aid)
+    cursor.execute(query)
+    item = cursor.fetchone()
+    print("auction_id: {0}".format(aid))
+    print("  degree: {0}".format(degree))
+    print("  title: {0}".format(item['title']))
+    # print("  description: {0}".format(item['description']))
